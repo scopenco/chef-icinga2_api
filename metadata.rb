ff34ab40fe9b1b986a75083bb3a0a755d8d47505
Andrei Skopenko <askopenko@malwarebytes.com>
2017-11-17 10:10:52 +0200

    Bump ver

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Chef LWRPs to interact with Icinga2 API'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/scopenco/chef-icinga2_api'
 issues_url 'https://github.com/scopenco/chef-icinga2_api/issues'
-version '0.1.1'
+version '0.1.2'
 
 supports 'amazon'
 supports 'redhat'
